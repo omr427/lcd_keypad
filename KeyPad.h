1ffeaeb1b48ea538d6021be0a550be0fbd6c1d19
omr427 <70269393+omr427@users.noreply.github.com>
2020-09-07 16:54:23 +0200

    assig

---
+++ KeyPad.h
@@ -0,0 +1,19 @@
+﻿/*
+ * IncFile1.h
+ *
+ * Created: 04/07/2020 01:40:36 م
+ *  Author: Ali
+ */ 
+
+
+#ifndef KeyPad_H
+#define KeyPad_H
+
+#include "KeyPad_Cfg.h"
+
+void KeyPad_Init(void);
+
+uint8 KeyPad_GetValue(void);
+
+
+#endif /* INCFILE1_H_ */
---
+++ KeyPad_Cfg.h
@@ -0,0 +1,28 @@
+﻿/*
+ * KeyPad_Cfg.h
+ *
+ * Created: 04/07/2020 01:41:19 م
+ *  Author: Ali
+ */ 
+
+
+#ifndef KEYPAD_CFG_H_
+#define KEYPAD_CFG_H_
+
+#include "DIO.h"
+
+#define  KEYPAD_PORT     DIO_PORTD
+
+#define  KEYPAD_COLUM_0  DIO_PIN0
+#define  KEYPAD_COLUM_1  DIO_PIN1
+#define  KEYPAD_COLUM_2  DIO_PIN2
+#define  KEYPAD_COLUM_3  DIO_PIN3
+
+#define  KEYPAD_ROW_0    DIO_PIN4
+#define  KEYPAD_ROW_1    DIO_PIN5
+#define  KEYPAD_ROW_2    DIO_PIN6
+#define  KEYPAD_ROW_3    DIO_PIN7
+
+
+
+#endif /* KEYPAD_CFG_H_ */
